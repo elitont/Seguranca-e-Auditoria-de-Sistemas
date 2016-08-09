3541554b1bb070c0b519bf8d047947f0b1fc04c5
elitont <eliton.traverssini@gmail.com>
2016-08-09 01:49:45 -0300

    criptografia por AES256

---
+++ AES 256/README.md
@@ -0,0 +1,5 @@
+# Crifagem e decifragem através do algoritmo de criptografia de blocos: AES 256
+
+by Eliton Traverssnini <eliton.traverssini@gmail.com>
+
+Exemplo de execução: python aes256.py Data/key16.in Data/data2.in
---
+++ AES 256/aes256.py
@@ -0,0 +1,27 @@
+from Crypto import Random
+from Crypto.Cipher import AES
+from sys import argv
+
+file = open(argv[1])
+chave = file.read()
+file.close()
+
+file = open(argv[2])
+texto = file.read()
+file.close()
+
+IV = Random.new().read(AES.block_size) # 16 bytes aleatorios
+
+try:
+	cipher = AES.new(chave, mode=AES.MODE_CFB, IV=IV)
+except ValueError as error:
+	print error
+	exit(0)
+
+cifrado = IV + cipher.encrypt(texto)
+decifrado = cipher.decrypt(cifrado)[AES.block_size:]
+
+print "\nTexto cifrado:"
+print cifrado
+print "\nTexto decifrado:"
+print decifrado
