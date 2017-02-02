c153d946cab35ab16218af9161651f8d9465ae15
elitont <eliton.traverssini@gmail.com>
2017-02-02 12:33:02 -0200

    Assinatura Digital: Gerando a assinatura, ok

--- Assinatura Digital/assinatura_digital.py
+++ Assinatura Digital/assinatura_digital.py
@@ -7,10 +7,27 @@ import binascii
 import base64
 import sys
 
-opt = sys.argv[1]
+opt = int(sys.argv[1])
 
 if opt == 1:
-	# text = sys.argv[2]
-	print("criar assinatura digital")
+	text = open(sys.argv[2], 'r+').read()
+	hash = SHA256.new()
+	hash.update(text.encode('utf-8'))
+	sha_256 = hash.digest()
+
+	random_generator = Random.new().read
+	key = RSA.generate(2048, random_generator)
+	public_key = key.publickey()
+	str_public_key = public_key.exportKey("PEM")
+	str_private_key = key.exportKey("PEM")
+	signature = public_key.encrypt(sha_256, 32)
+
+	fkey = open('cfg/chave_publica.pem', 'w')
+	fkey.write(str_public_key.decode('utf-8'))
+
+	fout = open('cfg/valida.in', 'w')
+	fout.write(str(text) + '\n' + str(sha_256) + '\n')
+	fout.write(str(signature) + '\n' + str(str_public_key))
+
 else:
 	print("validar assinatura digital")
