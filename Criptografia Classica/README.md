84763dccd1b17f7da14c29dcd1713432e52fe794
elitont <eliton.traverssini@gmail.com>
2017-02-02 14:52:12 -0200

    Criptografia Clássica: Implementação dos algoritmos de Vigenere, Ceasar, Transposição e Substituição

---
+++ Criptografia Classica/README.md
@@ -0,0 +1,5 @@
+Implementar as 4 cifras de criptografia clássica discutidas em aula:
+	Vigenere
+	Ceasar
+	Transposição 
+	Substituição
---
+++ Criptografia Classica/src/vigenere.py
@@ -0,0 +1,8 @@
+#!/opt/plone/zinstance/bin/python3
+# -*- coding: utf-8 -*-
+from Crypto.Hash import SHA256
+from Crypto.PublicKey import RSA
+from Crypto import Random
+import binascii
+import base64
+import sys
